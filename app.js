3ff712fb3203c2d3ef6b4d740733d6892ca40f1f
raphaeldesm <raphaeldesm@hotmail.com>
2024-05-21 15:12:15 -0400

    Deixando o jogo mais facil

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroMaximo = 100;
+let numeroMaximo = 50;
 let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 console.log(numeroSecreto);
 let chute;
